9fc7d951ad12f1f821f03f8f6bd89e2cc8b577c1
thaonguyenhuu251 <htnguyendev@gmail.com>
2023-09-10 15:18:03 +0700

    2 commit

--- src/main/java/com/htnguyen/demo/controllers/ProductControllers.java
+++ src/main/java/com/htnguyen/demo/controllers/ProductControllers.java
@@ -7,10 +7,7 @@ import com.htnguyen.demo.repositories.ProductRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -43,5 +40,18 @@ public class ProductControllers {
                         ""
                 )
         );
+    }
+    @PostMapping("/insert")
+    ResponseEntity<ResponseObject> insertProduct(@RequestBody Product newProduct) {
+        List<Product> foundProducts = productRepositories.findByProductName(newProduct.getProductName().trim());
+        if (foundProducts.size() > 0 ) {
+            return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(
+                    new ResponseObject("Failed", "This product already taken", "")
+            );
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    new ResponseObject("Success", "Insert product successfully", productRepositories.save(newProduct))
+            );
+        }
     }
 }
--- src/main/java/com/htnguyen/demo/repositories/ProductRepositories.java
+++ src/main/java/com/htnguyen/demo/repositories/ProductRepositories.java
@@ -4,7 +4,9 @@ import com.htnguyen.demo.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepositories extends JpaRepository<Product, Long> {
-
+    List<Product> findByProductName(String productName);
 }
